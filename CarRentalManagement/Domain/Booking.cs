8c65d27a7d9b457fcbfc94a63799d6d5c795de41
ENMVJ1663 <johnathonwoo1663@gmail.com>
2025-11-12 00:21:31 +0800

    Added database and seed data.

---
+++ CarRentalManagement/Domain/Booking.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalManagement.Domain
+{
+    public class Booking : BaseDomainModel
+    {
+        public DateTime DateOut { get; set; }
+        public DateTime DateIn { get; set; }
+        [ForeignKey("Vehicle")] public int VehicleId { get; set; }
+        [ForeignKey("Customer")] public int CustomerId { get; set; }
+        
+    }
+}
